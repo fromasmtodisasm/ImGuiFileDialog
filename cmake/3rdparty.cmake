3bffc0cb7795d1d72ffcd90aa42ad7da61275421
denis koveshnikov <chiappa@raa-st.com>
2020-07-15 14:43:14 +0300

    - converted to lib

--- cmake/3rdparty.cmake
+++ cmake/3rdparty.cmake
@@ -1,6 +1,3 @@
-set(OpenGL_GL_PREFERENCE GLVND)
-find_package(OpenGL REQUIRED)
-
 if (CMAKE_SYSTEM_NAME STREQUAL Linux)
   find_package(X11 REQUIRED)
 
@@ -8,7 +5,4 @@ if (CMAKE_SYSTEM_NAME STREQUAL Linux)
     message(FATAL_ERROR "X11 Xi library is required")
   endif ()
 endif ()
-
-include(cmake/glad.cmake)
-include(cmake/glfw.cmake)
 include(cmake/imgui.cmake)
